f18b8eda01259e7e9c7ad6672ebad5fc9f6cc866
rmdon <romdonofficial@gmail.com>
2024-10-02 20:56:39 +0700

    DataScienceEcosystem

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# - The popular languages that Data Scientists
+# - Libraries used by Data Scientists
+# - Data Science Tools
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. C++
+# 3. Java Script
+#     
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. Pandas
+# 2. Numpy
+# 3. Tensorflow
+
+# | Data Science Tools |
+# |--------------------|
+# | Jupyter Notebook |
+# | R Studio|
+# | Google Colab |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[5]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+
+(3*4)+5
+
+
+# In[6]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+
+200/60
+
+
+# ## Author
+# Muhammad Romdon Dahlan
